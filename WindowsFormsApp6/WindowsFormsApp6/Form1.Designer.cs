fc5f8a583262a3654fd55930a18d75067fba694c
RimasBerz <116100732+RimasBerz@users.noreply.github.com>
2022-11-14 23:53:04 +0200

    Add files via upload

---
+++ WindowsFormsApp6/WindowsFormsApp6/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿
+namespace WindowsFormsApp6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.sddsdToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dwsdToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dsdsdToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sdsdsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dsdsdToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.sdsdsToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.sdsdToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip2 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            this.menuStrip1.MouseEnter += new System.EventHandler(this.menuStrip1_MouseEnter);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1,
+            this.dwsdToolStripMenuItem,
+            this.dsdsdToolStripMenuItem,
+            this.sdsdsToolStripMenuItem,
+            this.dsdsdToolStripMenuItem1,
+            this.sdsdsToolStripMenuItem1,
+            this.sdsdToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(181, 158);
+            this.contextMenuStrip1.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStrip1_Opening);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sddsdToolStripMenuItem});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.toolStripMenuItem1.Text = "toolStripMenuItem1";
+            // 
+            // sddsdToolStripMenuItem
+            // 
+            this.sddsdToolStripMenuItem.Name = "sddsdToolStripMenuItem";
+            this.sddsdToolStripMenuItem.Size = new System.Drawing.Size(105, 22);
+            this.sddsdToolStripMenuItem.Text = "sddsd";
+            // 
+            // dwsdToolStripMenuItem
+            // 
+            this.dwsdToolStripMenuItem.Name = "dwsdToolStripMenuItem";
+            this.dwsdToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.dwsdToolStripMenuItem.Text = "dwsd";
+            // 
+            // dsdsdToolStripMenuItem
+            // 
+            this.dsdsdToolStripMenuItem.Name = "dsdsdToolStripMenuItem";
+            this.dsdsdToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.dsdsdToolStripMenuItem.Text = "dsdsd";
+            // 
+            // sdsdsToolStripMenuItem
+            // 
+            this.sdsdsToolStripMenuItem.Name = "sdsdsToolStripMenuItem";
+            this.sdsdsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.sdsdsToolStripMenuItem.Text = "sdsds";
+            // 
+            // dsdsdToolStripMenuItem1
+            // 
+            this.dsdsdToolStripMenuItem1.Name = "dsdsdToolStripMenuItem1";
+            this.dsdsdToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.dsdsdToolStripMenuItem1.Text = "dsdsd";
+            // 
+            // sdsdsToolStripMenuItem1
+            // 
+            this.sdsdsToolStripMenuItem1.Name = "sdsdsToolStripMenuItem1";
+            this.sdsdsToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.sdsdsToolStripMenuItem1.Text = "sdsds";
+            // 
+            // sdsdToolStripMenuItem
+            // 
+            this.sdsdToolStripMenuItem.Name = "sdsdToolStripMenuItem";
+            this.sdsdToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.sdsdToolStripMenuItem.Text = "sdsd";
+            // 
+            // contextMenuStrip2
+            // 
+            this.contextMenuStrip2.Name = "contextMenuStrip2";
+            this.contextMenuStrip2.Size = new System.Drawing.Size(61, 4);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Location = new System.Drawing.Point(0, 24);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(800, 25);
+            this.toolStrip1.TabIndex = 2;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem sddsdToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem dwsdToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem dsdsdToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sdsdsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem dsdsdToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem sdsdsToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem sdsdToolStripMenuItem;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip2;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+    }
+}
+
---
+++ WindowsFormsApp6/WindowsFormsApp6/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp6
+{
+    public partial class Form1 : Form
+    {
+        ToolStripMenuItem child;
+        public static int check = 1;
+        public int Check { get; set; }
+        public void CheckRemove()
+        {
+            check++;
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            child = new ToolStripMenuItem();
+            child.ShowDropDown(); 
+            int val = 1;
+            toolStripMenuItem1.Tag = val;
+            CheckRemove();
+            child.Visible = true;
+
+        }
+        private void contextMenuStrip1_Opening(object sender, CancelEventArgs e)
+        {
+            var b = new Button() { Parent = this, Text = Convert.ToString(check)};
+            Button s = sender as Button;
+            b.Text = Convert.ToString(check);
+        }
+
+        private void menuStrip1_MouseEnter(object sender, EventArgs e)
+        {
+            string check1 = check.ToString();
+            toolStrip1.Items.Add(child);
+            child.Text = check1;
+        }
+    }
+}
+
+
+
